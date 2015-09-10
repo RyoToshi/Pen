88123069d2bf266edde54e8ba1adb54251a50ce6
Bolkhovskiy Alexey <bolkhovskiy.a@gmail.com>
2015-09-10 07:45:37 +0300

    Второй коммит. добавлены функции автоматической ручки

--- pen.js
+++ pen.js
@@ -1,3 +1,8 @@
+/***
+ * Ручка
+ * @param color - цвет задается как в css
+ * @constructor
+ */
 function Pen(color) {
     this.color = color;
     this.inkValue = 100; //кол-во пасты в ручке
@@ -10,10 +15,10 @@ Pen.prototype.writeText = function (text) {
         colorfulConsole(text.slice(0, this.inkValue),this.color);
         this.inkValue -= text.length;
         if (this.inkValue === 0) {
-            console.log("Your writing tool is empty, recharge it");
+            console.log("Your pen is empty. please, recharge it");
         }
     } else {
-        console.log("Your writing tool is empty, recharge it")
+        console.log("Your pen is empty. please, recharge it")
     }
 };
 //======================Test===========================
@@ -21,13 +26,53 @@ Pen.prototype.writeText = function (text) {
 
 function testPen(it, times) {
     for (var i = 0; times > i; i++) {
-        it.writeText("Good by brutal world !!!");
+        it.writeText("Bye bye");
     }
 }
 
-console.log("____________Pen______________")
-console.log("=============================");
+/***
+ * Ручка становится автоматической
+ * @param color - цвет задается как в css
+ * @constructor
+ */
+function AutomaticPen(color) {
+    Pen.apply(this, arguments);
+    this.turnOn = false;
+    this.turn = function () {
+        return this.turnOn = !this.turnOn;
+    };
+}
+
+AutomaticPen.prototype = Object.create(Pen.prototype);
+AutomaticPen.prototype.constructor = AutomaticPen;
+
+
+AutomaticPen.prototype.writeText = function (text) {
+    if (this.turnOn) {
+        Pen.prototype.writeText.apply(this, arguments);
+    } else {
+        console.log("Pen is turned off, turnOn it and write");
+    }
+    return this;
+};
+
+
+
+
+console.log("             Pen             ")
+console.log("\n=============================");
 var pen = new Pen("red");
-testSupply(pen, 14);
+testPen(pen, 14);
 pen.rechargeInk();
-testSupply(pen, 10);
+testPen(pen, 10);
+
+console.log("      AutomaticPen             ")
+console.log("\n=============================");
+
+var ap = new AutomaticPen("green");
+console.log("Is Pen a prototype of AutomaticPen? "+Pen.prototype.isPrototypeOf(ap));
+testPen(ap, 11);
+ap.turn();
+testPen(ap, 10);
+ap.rechargeInk();
+testPen(ap, 2);
